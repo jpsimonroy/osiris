c64a1161f76d4be7e131a23d7fe2e7c59b1238ee
Ashok Gowtham M <ashokgo@thoughtworks.com>
2014-04-21 18:51:34 +0530

    [Ashok/Simon] go to usages implemented. smart enough to exclude declaration and/or current usage under cursor.

--- osiris.py
+++ osiris.py
@@ -8,19 +8,44 @@ index={}
 matches_from_index = []
 selected_word = ""
 page_index={}
+usages_index = {}
 
-class Reindexer(sublime_plugin.EventListener): 
-    def on_post_save(self, view):  
+class Reindexer(sublime_plugin.EventListener):
+    def on_post_save(self, view):
         global index
         index.clear()
         fs=getallfiles(cwd_for_window(sublime.active_window()))
         for f in fs:
-            build_index(f, index) 
+            build_index(f, index)
+
+class UsagesCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        global usages_index
+        global matches_from_index
+        index_if_needed()
+        usages_index.clear()
+        fs=getallfiles(cwd_for_window(sublime.active_window()))
+        word = current_word();
+        for f in fs:
+            build_usages_index(f, usages_index, word)
+        possible_matches = usages_index.get(word);
+        declarations = index.get(word)
+        matches_from_index = [z for z in filter(lambda x: (x[1], x[3]) not in [(d[1],d[3]) for d in declarations], possible_matches)]
+        print(matches_from_index)
+        if(matches_from_index == None):
+            return
+        if(len(matches_from_index) == 1):
+            model = matches_from_index[0]
+            view = self.window.open_file(model[3])
+            navigate(view, model)
+        elif(len(matches_from_index) > 1):
+            show_matches()
+
 
 class FileNavigatorCommand(sublime_plugin.WindowCommand):
     def run(self):
         global page_index
-        global matches_from_index  
+        global matches_from_index
         page_index = {}
         view = self.window.active_view()
         build_index(view.file_name(), page_index)
@@ -36,11 +61,8 @@ class TheNavigatorCommand(sublime_plugin.WindowCommand):
         global selected_word
         global index
         view = self.window.active_view()
-        selected_word=view.substr(view.word(view.sel()[0]))
-        if(len(index) == 0):
-            fs=getallfiles(cwd_for_window(self.window))
-            for f in fs:
-                build_index(f, index)    
+        selected_word=current_word()
+        index_if_needed()
         matches_from_index = index.get(selected_word)
         if(matches_from_index == None):
             return
@@ -50,6 +72,17 @@ class TheNavigatorCommand(sublime_plugin.WindowCommand):
             navigate(view, model)
         elif(len(matches_from_index) > 1):
             show_matches()
+
+def current_word(view=None):
+    view=view or sublime.active_window().active_view()
+    return(view.substr(view.word(view.sel()[0])))
+
+def index_if_needed():
+    global index
+    if(len(index) == 0):
+        fs=getallfiles(cwd_for_window(sublime.active_window()))
+        for f in fs:
+            build_index(f, index)
 
 def show_matches():
     names = []
@@ -87,14 +120,21 @@ def getallfiles(dir, extn = [".r", ".R"]):
                 fs.append(os.path.join(root, file))
     return fs
 
-def build_index(file, index):
+def build_index(file, index, search_pattern=r"([a-zA-Z0-9\_]+) *(=|<-|<<-) *(function\(.*?\))"):
     f=open(file,"r")
     text = f.read()
-    search_pattern = r"([a-zA-Z0-9\_]+) *(=|<-|<<-) *(function\(.*?\))"
     for match in re.finditer(search_pattern, text):
         index[match.group(1)] = index.get(match.group(1)) or []
         index[match.group(1)].append([match.group(1),match.start(),match.group(3),file])
     file_name, ext = os.path.splitext(os.path.basename(file))
     index[file_name] = index.get(file_name) or []
     index[file_name].append([file_name, 0,'FILE',file])
+    f.close()
+
+def build_usages_index(file, index, search_keyword):
+    f=open(file,"r")
+    text = f.read()
+    for match in re.finditer(search_keyword, text):
+        index[match.group(0)] = index.get(match.group(0)) or []
+        index[match.group(0)].append([match.group(0),match.start(),'',file])
     f.close()
