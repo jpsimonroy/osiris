8a963b40c90c2f71e6d73156bca1add2ba73f61a
Simon <simonroy@thoughtworks.com>
2014-04-21 17:47:15 +0530

    Super+F12 to list all methods in file.

--- osiris.py
+++ osiris.py
@@ -7,6 +7,7 @@ from .common import *
 index={}
 matches_from_index = []
 selected_word = ""
+page_index={}
 
 class Reindexer(sublime_plugin.EventListener): 
     def on_post_save(self, view):  
@@ -15,44 +16,59 @@ class Reindexer(sublime_plugin.EventListener):
         index.clear()
         fs=getallfiles(cwd_for_window(sublime.active_window()))
         for f in fs:
-            build_index(f)    
+            build_index(f, index) 
+
+class FileNavigatorCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        global page_index
+        global matches_from_index  
+        page_index = {}
+        view = self.window.active_view()
+        build_index(view.file_name(), page_index)
+        matches_from_index = []
+        for key in page_index:
+            for match in page_index[key]:
+                matches_from_index.append(match)
+        print(matches_from_index)
+        show_matches()
 
 class TheNavigatorCommand(sublime_plugin.WindowCommand):
     def run(self):
         global matches_from_index
         global selected_word
+        global index
         view = self.window.active_view()
         selected_word=view.substr(view.word(view.sel()[0]))
         if(len(index) == 0):
             fs=getallfiles(cwd_for_window(self.window))
             for f in fs:
-                build_index(f)    
+                build_index(f, index)    
         matches_from_index = index.get(selected_word)
         if(matches_from_index == None):
             return
         if(len(matches_from_index) == 1):
             model = matches_from_index[0]
-            view = self.window.open_file(model[2])
+            view = self.window.open_file(model[3])
             navigate(view, model)
         elif(len(matches_from_index) > 1):
-            self.show_matches(selected_word)
+            show_matches()
 
-    def show_matches(self, function_name):
-        names = []
-        for match in matches_from_index:
-            item = []
-            item.append(function_name + ":" + match[1])
-            item.append(match[2])
-            names.append(item)
-        self.window.show_quick_panel(names, self.match_selected)
+def show_matches():
+    names = []
+    for match in matches_from_index:
+        item = []
+        item.append(match[0] + "::" + match[2])
+        item.append(match[3])
+        names.append(item)
+    sublime.active_window().show_quick_panel(names, match_selected)
 
-    def match_selected(self, match):
-        global matches_from_index
-        if match == -1:
-            return
-        model = matches_from_index[match]
-        view = self.window.open_file(model[2])
-        navigate(view, model)
+def match_selected(match):
+    global matches_from_index
+    if match == -1:
+        return
+    model = matches_from_index[match]
+    view = sublime.active_window().open_file(model[3])
+    navigate(view, model)
 
 def navigate(view, model):
     def navigate_after_load():
@@ -60,8 +76,8 @@ def navigate(view, model):
             sublime.set_timeout(navigate_after_load,10)
             return
         view.sel().clear()
-        view.sel().add(sublime.Region(model[0], model[0] + len(selected_word)))
-        view.show_at_center(model[0])
+        view.sel().add(sublime.Region(model[1], model[1] + len(selected_word)))
+        view.show_at_center(model[1])
     navigate_after_load()
 
 def getallfiles(dir, extn = [".r", ".R"]):
@@ -73,16 +89,14 @@ def getallfiles(dir, extn = [".r", ".R"]):
                 fs.append(os.path.join(root, file))
     return fs
 
-def build_index(file):
-    global index
+def build_index(file, index):
     f=open(file,"r")
     text = f.read()
     search_pattern = r"([a-zA-Z0-9\_]+) *(=|<-|<<-) *(function\(.*?\))"
     for match in re.finditer(search_pattern, text):
         index[match.group(1)] = index.get(match.group(1)) or []
-        index[match.group(1)].append([match.start(),match.group(3),file])
+        index[match.group(1)].append([match.group(1),match.start(),match.group(3),file])
     file_name, ext = os.path.splitext(os.path.basename(file))
     index[file_name] = index.get(file_name) or []
-    print(file_name)
-    index[file_name].append([0,'FILE',file])
+    index[file_name].append([file_name, 0,'FILE',file])
     f.close()
