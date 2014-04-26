b0f0db60809d7b1960e60cee852191e180224b88
Simon <simonroy@thoughtworks.com>
2014-04-26 11:48:30 +0530

    Simon - Test that support when navigating test files.

--- osiris.py
+++ osiris.py
@@ -31,7 +31,6 @@ class UsagesCommand(sublime_plugin.WindowCommand):
         possible_matches = usages_index.get(word);
         declarations = index.get(word)
         matches_from_index = [z for z in filter(lambda x: (x[1], x[3]) not in [(d[1],d[3]) for d in declarations], possible_matches)]
-        print(matches_from_index)
         if(matches_from_index == None):
             return
         if(len(matches_from_index) == 1):
@@ -44,16 +43,37 @@ class UsagesCommand(sublime_plugin.WindowCommand):
 
 class FileNavigatorCommand(sublime_plugin.WindowCommand):
     def run(self):
+        view = self.window.active_view()
+        file_path=view.file_name()
+        base_file_name = os.path.basename(file_path)
+        if base_file_name.startswith("test"):
+            self.navigate_test()
+        else:
+            self.navigate_source() 
+        
+
+    def navigate_source(self):
         global page_index
         global matches_from_index
         page_index = {}
-        view = self.window.active_view()
-        build_index(view.file_name(), page_index)
+        build_index(self.window.active_view().file_name(), page_index)
         matches_from_index = []
         for key in page_index:
             for match in page_index[key]:
                 matches_from_index.append(match)
         show_matches()
+
+    def navigate_test(self):
+        global page_index
+        global matches_from_index
+        page_index = {}
+        matches_from_index = []
+        page_index = index_test(self.window.active_view().file_name(), {})
+        matches_from_index = []
+        for key in page_index:
+            matches_from_index.append(page_index.get(key))
+        matches_from_index = sorted(matches_from_index, key=lambda element: element[0])
+        show_matches()                
 
 class TheNavigatorCommand(sublime_plugin.WindowCommand):
     def run(self):
@@ -138,3 +158,42 @@ def build_usages_index(file, index, search_keyword):
         index[match.group(0)] = index.get(match.group(0)) or []
         index[match.group(0)].append([match.group(0),match.start(),'',file])
     f.close()
+
+def index_test(file, index):
+    f = open(file, "r")
+    text = f.read()
+    context_searcher = r"context *\(\"(.*)\"\)|context *\(\'(.*)\'\)"
+    context_locations = []
+    for match in re.finditer(context_searcher, text):
+        context_locations.append([match.group(1) or match.group(2), match.start()])
+    test_searcher = r"test_that *\(\"(.*)\"|test_that *\(\'(.*)\'"
+    test_locations = []
+    for match in re.finditer(test_searcher, text):
+        test_locations.append([match.group(1) or match.group(2), match.start()])
+
+    tests_with_contexts = context_for_tests(context_locations, test_locations)
+    for test in tests_with_contexts:
+        context = test[2] or ['None', None]
+        index[test[0]] = [context[0], test[1], test[0], file] 
+    return index
+    
+def context_for_tests(context_locations, test_locations):
+    return_index = {}
+    if len(context_locations) == 0:
+        for test in test_locations:
+            return_index[''] = return_index.get('') or []
+            return_index.get('').append(['', test[1], test[2]])
+        return return_index
+
+    def interpolator(test):
+        for context in reversed(context_locations):
+            if test[1] > context[1]:
+                return(test.append(context))
+        return(test.append(None))
+    [interpolator(test) for test in test_locations]
+    return(test_locations)
+        
+    
+    
+
+
